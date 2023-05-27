ca0d9562afef0b3481dc3a0c12914de141af5989
Chandra Bhan <chandra_bhan@readybytes.in>
2023-05-27 15:10:58 +0530

    add new program

---
+++ array_compare.c
@@ -0,0 +1,44 @@
+/*
+Problem Statement:
+
+Write a C program that takes two arrays as input and determines whether they are equal or not. 
+The program should compare the elements of both arrays and return true if they have the same elements in any order, and false otherwise.
+
+Inputs:
+
+Two arrays A[] and B[] of integers.
+The size of the arrays N, where N represents the number of elements in each array.
+Output:
+True if the arrays A[] and B[] are equal, false otherwise.
+
+Example:
+-------------
+Input:
+N = 3
+A[] = {1, 2, 5}
+B[] = {2, 5, 1}
+
+Output:
+True
+-------------
+Input:
+A[] = {1, 2, 5, 1}
+B[] = {2, 5, 1, 2}
+
+Output:
+False
+-------------
+Input:
+A[] = {1, 2, 5, 1}
+B[] = {2, 5, 1, 5}
+
+Output:
+False
+-------------
+Note:
+
+- The program should handle arrays of any size.
+- The order of elements in the arrays does not matter for determining equality.
+- The program should provide the correct output for all possible valid inputs.
+
+*/
